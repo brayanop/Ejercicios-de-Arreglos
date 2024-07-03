84aeea335f2e020bad9144e66bf2eda30af4fed2
#SONY_FOX <131622618+brayanop@users.noreply.github.com>
2024-07-03 12:58:35 -0500

    Update EJEC13cpp.cpp

--- EJEC13cpp.cpp
+++ EJEC13cpp.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 using namespace std;
-void mistrcpy(char str1[], char str2[]) {
-    int i = 0;
-    while (str2[i] != '\0') {
+void mistrcpy(string str1[], string str2[] , int N) {
+    for (int i = 0;i < N;i++){
         str1[i] = str2[i];
-        i++;
     }
-    str1[i] = '\0';
 }
 
 int main() {
-    char A[100];
-    char B[] = "palabra copiado en cadena 2"; 
-    mistrcpy(B,A);
-    cout << "Cadena copiada: " << A << endl;
+    string A[100];
+    string B[100]; 
+    int cont = 1;
+    cout << "Ingrese los elementos de su arreglo A (escribe '/' para terminar):" << endl;
+    for (int i = 0; i < 100; i++) {
+        cin >> A[i];
+        if (A[i] == "/") {
+        	cont--;
+            break; 
+        } 
+        cont++;
+    }
     
+    mistrcpy(B,A,cont);
+    
+    cout << "Cadena copiada en arreglo B: " << endl ;
+    for(int i = 0 ;i<cont;i++){
+    	cout<< B[i] << " " ;
+	}
     return 0;
 }
